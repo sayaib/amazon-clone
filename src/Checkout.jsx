6f4ba0be5be2200420f90c2bc63ddbef585a4231
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-23 16:28:08 +0530

    checkout cart create

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from './Subtotal'
 
 function Checkout() {
   return (
@@ -13,7 +14,7 @@ function Checkout() {
           </div>
       </div>
       <div className="checkout_right">
-          <h2>The sub total</h2>
+          <Subtotal/>
       </div>
     </div>
   );
