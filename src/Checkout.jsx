31119e5549987865e4c00bd485b7035971a4aeec
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-23 15:57:42 +0530

    link to page

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -5,14 +5,16 @@ function Checkout() {
   return (
     <div className="checkout">
       <div className="checkout_left">
-          <img className="" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/AdvantagePrime/GW/Catpage_Nov1st/D25733060_IN_WLME_Advantage-Justfor-Prime_Mob_ingress-banner_1242x450.jpg" alt="" />
+          <img className="checkout_ad" src="https://m.media-amazon.com/images/I/41dtHRtxo3L._SX1500_.jpg" alt="" />
           <div>
               <h2 className="checkout_title">
                   Your Shopping Cart
               </h2>
           </div>
       </div>
-      <div className="checkout_right"></div>
+      <div className="checkout_right">
+          <h2>The sub total</h2>
+      </div>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header_logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazon-logo"
-      />
+      <Link to="/">
+        <img
+          className="header_logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon-logo"
+        />
+      </Link>
+
       <div className="header_search">
         <input type="text" className="header_searchInput" />
         <SearchIcon className="header_searchIcon" />
@@ -29,11 +33,12 @@ function Header() {
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
         </div>
-
-        <div className="header_optionBasket">
-          <ShoppingCartIcon />
-          <span className="header_optionLineTwo header_basketCount">0</span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingCartIcon />
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
