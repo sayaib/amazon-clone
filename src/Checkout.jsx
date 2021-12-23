dab3f401a02586ef508346caed9b0ca26b4d256b
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-23 15:42:46 +0530

    add checkout.css

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Checkout.css";
 
 function Checkout() {
-    return (
-        <div>
-            <h1>I am checkout</h1>
-        </div>
-    )
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+          <img className="" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/AdvantagePrime/GW/Catpage_Nov1st/D25733060_IN_WLME_Advantage-Justfor-Prime_Mob_ingress-banner_1242x450.jpg" alt="" />
+          <div>
+              <h2 className="checkout_title">
+                  Your Shopping Cart
+              </h2>
+          </div>
+      </div>
+      <div className="checkout_right"></div>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
