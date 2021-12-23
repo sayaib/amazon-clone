954eeeb2ca913969c8da81752189de7499e5faaa
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-23 15:23:17 +0530

    update browserrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css'
 import Header from './Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home'
+import Checkout from './Checkout'
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Home/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route component={Error} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
